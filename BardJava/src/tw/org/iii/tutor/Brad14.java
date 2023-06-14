6de9cb2b415c268cedbd9c48e3ecc8a7d6f058d5
User <User@WS21.r1802.tciii.org.tw>
2023-06-14 14:19:11 +0800

    0614

--- BardJava/src/tw/org/iii/tutor/Brad14.java
+++ BardJava/src/tw/org/iii/tutor/Brad14.java
@@ -1,9 +1,38 @@
 package tw.org.iii.tutor;
 
 public class Brad14 {
-public static void main(String[] args) {
-	int[] a=new int[10];
-	
-	
-}
+	public static void main(String[] args) {
+		int[] a = new int[10];
+		System.out.println(a.length);
+		int[] b = new int[5];
+		// a[1]=123;
+		// a[0]=763;
+		for (int i = 0; i < a.length; i++) {
+			a[i] = 10;
+		}
+		// for each 迴圈
+		for (int c : a) {
+			//System.out.println(c);
+		}
+		// int one = 0, two = 0, three = 0, four = 0, five = 0, six = 0;
+
+		int[] number = new int[7];
+		for (int c : number) {
+			number[c] = 0;
+		}
+		/////////////////////////////////// 骰子問題
+		for (int i = 0; i <= 100; i++) {
+			int c = (int) (Math.random() * 6) + 1;
+			//System.out.println(c);
+			number[c] += 1;
+		}
+		/*
+		 * for (int c : number) { System.out.printf("%d 點出現 %d 次", d, c); d += 1; }
+		 */
+		for (int i = 1; i <= 6; i++) {
+			System.out.printf("%d點出現%d次\t", i, number[i]);
+		}
+		/////////////////////////////////////
+
+	}
 }
