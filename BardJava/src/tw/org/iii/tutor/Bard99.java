1f3e0739ffda6d8ca2c339a20c86c23d6279296f
User <User@WS21.r1802.tciii.org.tw>
2023-06-14 13:33:55 +0800

    0614

--- BardJava/src/tw/org/iii/tutor/Bard99.java
+++ BardJava/src/tw/org/iii/tutor/Bard99.java
@@ -2,12 +2,16 @@ package tw.org.iii.tutor;
 
 public class Bard99 {
 	public static void main(String[] args) {
-		for (int a = 2; a < 10; a += 1) {
+		for (int a = 1; a < 10; a += 1) {
+			System.out.println();
 			for (int b = 1; b < 10; b += 1) {
-				System.out.printf("%d*%d=%d\t", a, b, a * b);
-				//System.out.println();
+				System.out.printf("%d*%d=%d\t", b, a, a * b);
+				
 
 			}
 		}
-	}
+		//質數
+			
+		}
+	
 }
---
+++ BardJava/src/tw/org/iii/tutor/Brad14.java
@@ -0,0 +1,9 @@
+package tw.org.iii.tutor;
+
+public class Brad14 {
+public static void main(String[] args) {
+	int[] a=new int[10];
+	
+	
+}
+}
