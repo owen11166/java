ddb257dab41a0347e6816f6ab3d000a144e53173
User <User@WS21.r1802.tciii.org.tw>
2023-07-03 16:32:05 +0800

    0703

--- BardJava/src/tw/org/iii/tutor/JDBC03.java
+++ BardJava/src/tw/org/iii/tutor/JDBC03.java
@@ -3,15 +3,19 @@ package tw.org.iii.tutor;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Properties;
 
-public class JDBC01 {
+public class JDBC03 {
 
 	public static void main(String[] args) {
 		try {
+			Properties prop = new Properties();
+			prop.put("user", "root");
+			prop.put("password", "root");
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/iii", "root", "root");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/iii", prop);
 			Statement stmt = conn.createStatement();
-			int n= stmt.executeUpdate("INSERT INTO cust(cname,tel,birthday)VALUES('PETER','321','1999-10-01')");
+			int n = stmt.executeUpdate("INSERT INTO cust(cname,tel,birthday)VALUES('owen','321','1999-10-01')");
 			System.out.println(n);
 		} catch (Exception e) {
 
