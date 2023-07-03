2851c74cc9a23a12d1292279777ea4cb0b5a9d81
User <User@WS21.r1802.tciii.org.tw>
2023-07-03 13:36:09 +0800

    0703

--- BardJava/src/tw/org/iii/tutor/Brad64.java
+++ BardJava/src/tw/org/iii/tutor/Brad64.java
@@ -5,22 +5,31 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 
-
 public class Brad64 {
 
 	public static void main(String[] args) {
-		byte[] data=new byte[1024];
-		try {
-			DatagramSocket socket=new DatagramSocket(8888);
-			DatagramPacket packet=new DatagramPacket(data,data.length);
-			socket.receive(packet);
-			socket.close();
-			System.out.println(new String(packet.getData()));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		while (true) {
+			byte[] buf = new byte[1024];
+			try {
+				DatagramSocket socket = new DatagramSocket(8888);
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+				socket.close();
 
+				InetAddress urip = packet.getAddress();
+				int len = packet.getLength();
+				byte[] data = packet.getData();
+				String mesg = new String(data, 0, len);
+
+				System.out.println(urip.getHostAddress() + ":" + mesg);
+				if (mesg.equals("quit")) {
+					
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
