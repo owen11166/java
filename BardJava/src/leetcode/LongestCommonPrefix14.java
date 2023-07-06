2270d3d5706fce24909f031d86378c9a5b20870a
User <User@WS21.r1802.tciii.org.tw>
2023-07-06 15:39:52 +0800

    0706

--- BardJava/src/leetcode/LongestCommonPrefix14.java
+++ BardJava/src/leetcode/LongestCommonPrefix14.java
@@ -6,9 +6,18 @@ public class LongestCommonPrefix14 {
 
 	};
 
-	public static String longestCommonPrefix(String[] strs) {
-
-		return "1";
-	};
-
+	public class Solution {
+	    public String longestCommonPrefix(String[] strs) {
+	        String res = "";
+	        for (int i = 0; i < strs[0].length(); i++) {
+	            for (String s : strs) {
+	                if (i == s.length() || s.charAt(i) != strs[0].charAt(i)) {
+	                    return res;
+	                }
+	            }
+	            res += strs[0].charAt(i);
+	        }
+	        return res;
+	    }
+	}
 }
---
+++ BardJava/src/leetcode/ValidParentheses20.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class ValidParentheses20 {
+
+	public static void main(String[] args) {
+
+	}
+
+	class Solution {
+		public static boolean isValid(String s) {
+			
+			return true;
+		}
+	}
+
+}
